56d79dcb8c365e85d47e920481d7c129d4070ae7
Nick <nick.valverde@epn.edu.ec>
2025-05-21 15:11:22 -0500

    Update: cambios no sincronizados

--- calculadora/src/app.service.ts
+++ calculadora/src/app.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    /*console.log('Hello World!');
+    console.debug('Hello World!');
+    console.info('Hello World!');
+    console.warn('Hello World!');
+    console.error('Hello World!');*/
+
+    return 'Hello World!!';
   }
 }
